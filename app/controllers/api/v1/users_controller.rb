df66ec4e2778e943880328031423bcc6a71fadfe
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-26 20:19:59 -0400

    took out class variable signs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
-    @user = User.new(username: params[:username], password: params[:password])
-    if @user.save
-      payload = {user_id: @user.id}
-      render json: {user: @user, jwt: issue_token(payload)}
+    user = User.new(username: params[:username], password: params[:password])
+    if user.save
+      payload = {user_id: user.id}
+      render json: {user: user, jwt: issue_token(payload)}
     else
 
     end
